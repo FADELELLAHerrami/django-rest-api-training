b6bf0a6b5e116320cbe6855271d1f3225a2c79eb
Fadel Ellah ERRAMI <94233481+FADELELLAHerrami@users.noreply.github.com>
2024-07-07 13:11:31 +0100

    why djangorestapi

--- restproject/restapp/urls.py
+++ restproject/restapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_all_courses
+from .views import get_all_courses, get_one_course
 
 urlpatterns = [
     path('courses/', get_all_courses, name = "course-list"),
+    path('course/<int:pk>', get_one_course, name = "course")
 ]
--- restproject/restapp/views.py
+++ restproject/restapp/views.py
@@ -9,3 +9,12 @@ def get_all_courses(request):
     courses = Course.objects.all()
     data = {"courses": list(courses.values())}
     return JsonResponse(data)
+def get_one_course(request, pk):
+    course = Course.objects.get(pk=pk)
+    data = {
+        'id': course.id,
+        'title': course.title,
+        'description': course.desc,
+        'published': course.active
+    }
+    return JsonResponse(data)
